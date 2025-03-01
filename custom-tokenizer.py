ecc6d69e326bed086ca95945c1a1d3c925226ba6
manuelrigobello <manuel.rigobello27@gmail.com>
2025-03-01 21:42:09 +0100

    run lg with updated tokenizer during train

--- custom-tokenizer.py
+++ custom-tokenizer.py
@@ -8,20 +8,15 @@ import spacy.training
 @spacy.registry.tokenizers("custom_tokenizer")
 def custom_tokenizer():
   def custom_tokenizer(nlp):
-    my_punct = f"[{re.escape(string.punctuation)}]"
-    my_punct = my_punct.replace("<", "")
-    my_punct = my_punct.replace(">", "")
-    html_tag_pattern = re.compile(r'<[^>]+>')
+    prefix_re = re.compile(r'[.,;:?!(\[\'"</]')
+    suffix_re = re.compile(r'[.,;:?!)\]\'">■™]')
+    infix_re = re.compile(r'[-/+=&]')
 
-    all_prefixes_re = spacy.util.compile_prefix_regex(tuple(list(nlp.Defaults.prefixes) + [html_tag_pattern.pattern] + [my_punct]))
-    infix_re = spacy.util.compile_infix_regex(nlp.Defaults.infixes)
-    suffix_re = spacy.util.compile_suffix_regex(tuple(list(nlp.Defaults.suffixes) + [html_tag_pattern.pattern] + [my_punct]))
-
-    return Tokenizer(nlp.vocab,
-                      nlp.Defaults.tokenizer_exceptions,
-                      prefix_search = all_prefixes_re.search, 
-                      infix_finditer = infix_re.finditer,
-                      suffix_search = suffix_re.search,
-                      token_match=None)
+    return Tokenizer(
+          nlp.vocab,
+          prefix_search=prefix_re.search,
+          suffix_search=suffix_re.search,
+          infix_finditer=infix_re.finditer
+      )
     
   return custom_tokenizer
